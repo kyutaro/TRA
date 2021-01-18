4b845a35696c5e3e0fb446eb4fe99a00511794ad
kyutaro <yoshithune1192@gmail.com>
2021-01-18 23:34:23 +0900

    backend：API処理の実装

--- backend/controller/project.js
+++ backend/controller/project.js
@@ -2,27 +2,33 @@ const express = require('express');
 const app = express();
 const systemLogger = require ('../config/log4js-config');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const Project = require('./../model/project');
 
-mongoose.connect('mongodb://localhost:27017/TRA', { useNewUrlParser: true, useUnifiedTopology: true });
-app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json());
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended:false }));
+// app.use(bodyParser.json());
 
 module.exports = {
   getProjectData: function (res) {
-    systemLogger.debug("getProjectDataの中");
-    Project.find({task: "test"}, function(err, docs) {
+    mongoose.connect('mongodb://localhost:27017/TRA', { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    Project.find({task: "test"}, (err, ret) => {
       if (err) console.error(err);
-      console.log(docs);
-      for (let val of docs) {
+      for (let val of ret) {
+        console.log("中身：");
         console.log(val);
-        mongoose.disconnect();
       }
-  
-        // systemLogger.debug("getProjectData：成功");
-        // systemLogger.debug(result);
-        // return res.json(result);
+      mongoose.disconnect();
+      return res.send(ret);
+
     });
   },
+  addProject: function (res) {
+    const projectSave = new Project({project_id:2, task:"test2", worktime:"00:00:00", del_flg:0});
+    projectSave.save(err => {
+      if (err) console.error(err)
+      console.log('saved')
+      mongoose.disconnect();
+    });
+  }
 };
